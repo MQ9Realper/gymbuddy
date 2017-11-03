3b2ee33beb4024479640868d09020ccc16ac5e5f
xts400 <dennis@threadstack.co>
2017-11-03 11:09:24 +0300

    Additions and refactors

--- app/src/main/java/com/gymbuddy/gymbuddy/activities/MainActivity.java
+++ app/src/main/java/com/gymbuddy/gymbuddy/activities/MainActivity.java
@@ -1,14 +1,44 @@
 package com.gymbuddy.gymbuddy.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import com.gymbuddy.gymbuddy.R;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.gymbuddy.gymbuddy.R;
+import com.gymbuddy.gymbuddy.views.Btn;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        Btn btnSignIn = (Btn) findViewById(R.id.btnSignIn);
+        Btn btnSignUp = (Btn) findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSignIn:
+                Intent intentSignIn = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intentSignIn);
+                break;
+            case R.id.btnSignUp:
+                Intent intentSignUp = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intentSignUp);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/gymbuddy/gymbuddy/activities/SignInActivity.java
@@ -0,0 +1,41 @@
+package com.gymbuddy.gymbuddy.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.gymbuddy.gymbuddy.R;
+import com.gymbuddy.gymbuddy.utils.UniversalUtils;
+
+public class SignInActivity extends AppCompatActivity {
+    private UniversalUtils universalUtils;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        universalUtils = new UniversalUtils(this);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarSignIn);
+        toolbar.setTitle("Sign In");
+        toolbar.setNavigationIcon(R.drawable.ic_keyboard_arrow_left_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        universalUtils.centerToolbarTitle(toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
